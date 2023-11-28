ee3ff9df397b4db0763d8a6db7d8f5c0396389fe
None <None>
2023-11-28 02:27:18 -0800

    Added p4 installation script

---
+++ install_p4.sh
@@ -0,0 +1,5 @@
+source /etc/lsb-release
+echo "deb http://download.opensuse.org/repositories/home:/p4lang/xUbuntu_${DISTRIB_RELEASE}/ /" | sudo tee /etc/apt/sources.list.d/home:p4lang.list
+curl -fsSL https://download.opensuse.org/repositories/home:p4lang/xUbuntu_${DISTRIB_RELEASE}/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/home_p4lang.gpg > /dev/null
+sudo apt-get update
+sudo apt install p4lang-p4c
